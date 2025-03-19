b5daa010d07fc3c1b9c8ce02cd4f6681b303d13e
Abhay <abhay@cleardigital.com>
2025-03-19 18:48:16 +0530

    update about section

--- next-app-portfolio/components/about.jsx
+++ next-app-portfolio/components/about.jsx
@@ -8,22 +8,26 @@ const About = () => {
           About
         </h4>
         <div className="wrapper w-full relative flex ">
-          <div className="heading-wrap relative max-w-[500px] uppercase">
-            <h4>Crafting User Interfaces: My Expertise</h4>
+          <div className="heading-wrapper relative  w-[50%]">
+            <div className="heading-wrap uppercase max-w-[500px] pr-[50px!important]">
+              <h4>Crafting User Interfaces: My Expertise</h4>
+            </div>
           </div>
-          <div className="text-wrap max-w-[500px]">
-            <p className="davy-black">
-              I am a web developer at{" "}
-              <Link
-                href="https://www.cleardigital.com"
-                className="vivid-blue"
-                target="_blank"
-              >
-                Clear Digital,
-              </Link>{" "}
-              where I indulge in the art of web design, striving to deliver
-              engaging and user-centric digital solutions.
-            </p>
+          <div className="text-wrap  w-[50%]">
+            <div className="text max-w-[500px]">
+              <p className="davy-black">
+                I am a web developer at{" "}
+                <Link
+                  href="https://www.cleardigital.com"
+                  className="vivid-blue"
+                  target="_blank"
+                >
+                  Clear Digital,
+                </Link>{" "}
+                where I indulge in the art of web design, striving to deliver
+                engaging and user-centric digital solutions.
+              </p>
+            </div>
           </div>
         </div>
       </div>
