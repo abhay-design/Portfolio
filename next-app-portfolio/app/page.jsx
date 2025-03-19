ca895ea3c0eee66e8dd7d64184cac644cf6a3220
Abhay <abhay@cleardigital.com>
2025-03-19 17:15:23 +0530

    update banner

--- next-app-portfolio/app/page.jsx
+++ next-app-portfolio/app/page.jsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import { Poppins } from "next/font/google";
 import Banner from "../components/banner";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
-    <div>
+    <div className={poppins.className}>
       <Banner />
     </div>
   );
--- next-app-portfolio/components/banner.jsx
+++ next-app-portfolio/components/banner.jsx
@@ -1,13 +1,49 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Banner = () => {
   return (
     <section className="banner w-full">
       <div className="container">
         <div className="wrapper w-full flex flex-wrap">
           <div className="text-wrap relative max-w-[600px]">
-            <h1 className="uppercase">
+            <h1 className="uppercase mb-[40px!important]">
               Hi, <br /> I'm <span className="vivid-blue">Abhay</span> <br />{" "}
               Web Dev
             </h1>
+            <div className="btn-wrap mb-[25px!important]">
+              <Link href="#" className="button">
+                Contact
+              </Link>
+            </div>
+            <div className="link-wrap">
+              <ul className="flex flex-wrap">
+                <li className="max-w-[24px] relative mr-[25px!important]">
+                  <Link href="#" className="emptylink"></Link>
+                  <Image
+                    src="/svg/icons8-linkedin.svg"
+                    width={1000}
+                    height={1000}
+                    className="w-full h-full object-contain"
+                    alt="linkdin"
+                  />
+                </li>
+                <li className="max-w-[24px] relative">
+                  <Link
+                    href="https://github.com/abhay-design"
+                    target="_blank"
+                    className="emptylink"
+                  ></Link>
+                  <Image
+                    src="/svg/icons8-github.svg"
+                    width={1000}
+                    height={1000}
+                    className="w-full h-full object-contain"
+                    alt="github"
+                  />
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
