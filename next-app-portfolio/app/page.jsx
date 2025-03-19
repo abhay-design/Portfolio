21c97b52375e7af49c0f483d3561a1f7de4ea943
Abhay <abhay@cleardigital.com>
2025-03-19 16:03:51 +0530

    update header

--- next-app-portfolio/app/page.jsx
+++ next-app-portfolio/app/page.jsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Banner from "../components/banner";
 
 export default function Home() {
-  return;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 }
---
+++ next-app-portfolio/components/banner.jsx
@@ -0,0 +1,17 @@
+const Banner = () => {
+  return (
+    <section className="banner w-full">
+      <div className="container">
+        <div className="wrapper w-full flex flex-wrap">
+          <div className="text-wrap relative max-w-[600px]">
+            <h1 className="uppercase">
+              Hi, <br /> I'm <span className="vivid-blue">Abhay</span> <br />{" "}
+              Web Dev
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Banner;
