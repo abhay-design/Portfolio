cd63ea1cbcf2d9b5e40dc55c4b2871e1c2ace5fd
Abhay <abhay@cleardigital.com>
2025-03-19 14:13:22 +0530

    added active link on header

--- next-app-portfolio/components/header/header.jsx
+++ next-app-portfolio/components/header/header.jsx
@@ -1,6 +1,16 @@
+"use client";
+
 import Link from "next/link";
+import Styles from "./header.module.css";
+import { useState } from "react";
 
 const Header = () => {
+  const [activelink, setactivelink] = useState("Home");
+
+  const Handlelinkclick = (linkname) => {
+    setactivelink(linkname);
+  };
+
   return (
     <header className="w-full pt-[20px!important] pb-[20px!important] bg-white z-[20] top-0 relative shadow">
       <div className="container">
@@ -13,27 +23,57 @@ const Header = () => {
           <div className="list-wrap relative">
             <ul className="flex flex-wrap">
               <li className="mr-[30px!important]">
-                <Link href="#" className="uppercase text-[14px] font-bold">
+                <Link
+                  href="#"
+                  className={`uppercase relative text-[14px] font-bold  ${
+                    activelink === "Home" ? "active" : ""
+                  }`}
+                  onClick={() => Handlelinkclick("Home")}
+                >
                   Home
                 </Link>
               </li>
               <li className="mr-[30px!important]">
-                <Link href="#" className="uppercase text-[14px] font-bold">
+                <Link
+                  href="#"
+                  className={`uppercase relative text-[14px] font-bold  ${
+                    activelink === "About" ? "active" : ""
+                  }`}
+                  onClick={() => Handlelinkclick("About")}
+                >
                   About
                 </Link>
               </li>
               <li className="mr-[30px!important]">
-                <Link href="#" className="uppercase text-[14px] font-bold">
+                <Link
+                  href="#"
+                  className={`uppercase relative text-[14px] font-bold  ${
+                    activelink === "skills" ? "active" : ""
+                  }`}
+                  onClick={() => Handlelinkclick("skills")}
+                >
                   skills
                 </Link>
               </li>
               <li className="mr-[30px!important]">
-                <Link href="#" className="uppercase text-[14px] font-bold">
+                <Link
+                  href="#"
+                  className={`uppercase relative text-[14px] font-bold  ${
+                    activelink === "projects" ? "active" : ""
+                  }`}
+                  onClick={() => Handlelinkclick("projects")}
+                >
                   projects
                 </Link>
               </li>
               <li>
-                <Link href="#" className="uppercase text-[14px] font-bold">
+                <Link
+                  href="#"
+                  className={`uppercase relative text-[14px] font-bold  ${
+                    activelink === "contact" ? "active" : ""
+                  }`}
+                  onClick={() => Handlelinkclick("contact")}
+                >
                   contact
                 </Link>
               </li>
